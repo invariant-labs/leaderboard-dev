3ae168be789fe9dee22f26bc696f61dd86b6a0c0
matepal00 <mateusz.paluch2311@gmail.com>
2024-12-06 14:48:08 +0100

    requested changes

--- src/snap-points.ts
+++ src/snap-points.ts
@@ -1,137 +1,22 @@
 import {
   Network,
   Market,
-  Pair,
   getMarketAddress,
   IWallet,
-  InvariantEventNames,
-  parseEvent,
 } from "@invariant-labs/sdk-eclipse";
-import {
-  CreatePositionEvent,
-  PoolStructure,
-} from "@invariant-labs/sdk-eclipse/lib/market";
 import { AnchorProvider } from "@coral-xyz/anchor";
-import BN from "bn.js";
-import {
-  ConfirmedSignatureInfo,
-  Connection,
-  ParsedTransactionWithMeta,
-  PublicKey,
-} from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 import fs from "fs";
 import path from "path";
 import { MAX_SIGNATURES_PER_CALL } from "./consts";
+import {
+  fetchAllSignatures,
+  extractEvents,
+  fetchTransactionLogs,
+} from "./utils";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
-
-const fetchAllSignatures = async (
-  connection: Connection,
-  programId: PublicKey,
-  lastTxHash: string | undefined
-) => {
-  const allSignatures: ConfirmedSignatureInfo[] = [];
-  let beforeTxHash: string | undefined = undefined;
-  let done: boolean = false;
-
-  while (!done) {
-    const signatures = await connection.getSignaturesForAddress(
-      programId,
-      { before: beforeTxHash, until: lastTxHash },
-      "confirmed"
-    );
-
-    if (signatures.length === 0) {
-      done = true;
-      break;
-    }
-
-    allSignatures.push(...signatures);
-    if (lastTxHash === undefined) {
-      done = true;
-      break;
-    }
-    if (signatures[signatures.length - 1].signature === lastTxHash) {
-      done = true;
-    } else {
-      beforeTxHash = signatures[signatures.length - 1].signature;
-    }
-  }
-
-  return allSignatures.map((signatureInfo) => signatureInfo.signature);
-};
-
-const processParsedTransactions = (
-  parsedTransactions: (ParsedTransactionWithMeta | null)[]
-) => {
-  return parsedTransactions
-    .filter((tx) => tx?.meta?.logMessages && tx.transaction.signatures[0])
-    .map((tx) => {
-      return tx!.meta!.logMessages!;
-    });
-};
-
-const fetchTransactionLogs = async (
-  connection: Connection,
-  signatures: string[],
-  batchSize: number
-) => {
-  const batchCount = Math.ceil(signatures.length / batchSize);
-  const batchedSignatures = new Array(batchCount).fill(0);
-
-  return (
-    await Promise.all(
-      batchedSignatures.map(async (_, idx) => {
-        const batchSignatures = signatures.slice(
-          idx * batchSize,
-          (idx + 1) * batchSize
-        );
-        return processParsedTransactions(
-          await connection.getParsedTransactions(batchSignatures, "confirmed")
-        );
-      })
-    )
-  ).flat();
-};
-
-const extractEvents = (
-  initialEvents: any,
-  market: Market,
-  transactionLog: string[]
-) => {
-  const eventsObject = initialEvents;
-  console.log(initialEvents);
-  const eventLogs = transactionLog.filter((log) =>
-    log.startsWith("Program data:")
-  );
-  eventLogs.forEach((eventLog) => {
-    const decodedEvent = market.eventDecoder.decode(
-      eventLog.split("Program data: ")[1]
-    );
-    if (!decodedEvent) {
-      return;
-    }
-
-    switch (decodedEvent.name) {
-      case InvariantEventNames.CreatePositionEvent:
-        eventsObject[InvariantEventNames.CreatePositionEvent].push(
-          parseEvent(decodedEvent)
-        );
-        console.log(parseEvent(decodedEvent));
-        break;
-      case InvariantEventNames.RemovePositionEvent:
-        eventsObject[InvariantEventNames.RemovePositionEvent].push(
-          parseEvent(decodedEvent)
-        );
-        console.log(parseEvent(decodedEvent));
-        break;
-      default:
-        return;
-    }
-  });
-  return eventsObject;
-};
 
 export const createSnapshotForNetwork = async (network: Network) => {
   let provider: AnchorProvider;
@@ -142,9 +27,15 @@ export const createSnapshotForNetwork = async (network: Network) => {
   switch (network) {
     case Network.MAIN:
       provider = AnchorProvider.local("https://eclipse.helius-rpc.com");
-      lastTxHashFileName = "../data/eclipse/points/last_tx_hash_mainnet.json";
-      eventsSnapFilename = "../data/eclipse/points/events_snap_mainnet.json";
-      pointsFileName = "../data/eclipse/points/points_mainnet.json";
+      lastTxHashFileName = path.join(
+        __dirname,
+        "../data/last_tx_hash_mainnet.json"
+      );
+      eventsSnapFilename = path.join(
+        __dirname,
+        "../data/events_snap_mainnet.json"
+      );
+      pointsFileName = path.join(__dirname, "../data/points_mainnet.json");
       break;
     case Network.TEST:
       provider = AnchorProvider.local(
@@ -178,7 +69,11 @@ export const createSnapshotForNetwork = async (network: Network) => {
     JSON.parse(fs.readFileSync(lastTxHashFileName, "utf-8")).lastTxHash ??
     undefined;
 
-  const sigs = await fetchAllSignatures(connection, programId, lastTxHash);
+  const sigs = await fetchAllSignatures(
+    connection,
+    market.eventOptAccount.address,
+    lastTxHash
+  );
   if (sigs.length === 0) return;
 
   const data = { lastTxHash: sigs[0] };
