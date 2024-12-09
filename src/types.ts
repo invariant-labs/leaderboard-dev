f856367f6b3e5acee7a53b8ab90ed0394a36db20
matepal00 <mateusz.paluch2311@gmail.com>
2024-12-09 19:32:52 +0100

    code refactor

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,30 @@
+import { BN } from "@coral-xyz/anchor";
 import {
   CreatePositionEvent,
+  PoolStructure,
   RemovePositionEvent,
+  Tick,
 } from "@invariant-labs/sdk-eclipse/lib/market";
+import { PublicKey } from "@solana/web3.js";
 
 export interface IActive {
   event: CreatePositionEvent;
-  points: number;
+  points: BN;
 }
 export interface IClosed {
   events: [CreatePositionEvent | null, RemovePositionEvent];
-  points: number;
+  points: BN;
 }
 export interface IPositions {
   active: IActive[];
   closed: IClosed[];
+}
+export interface IConfig {
+  lastTxHash: string;
+  lastSnapTimestamp: string;
+}
+export interface IPoolAndTicks {
+  pool: PublicKey;
+  poolStructure: PoolStructure;
+  ticks: Tick[];
 }
