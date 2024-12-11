13717c7c524f1de9ec2bd0620418e64985aa2124
Sniezka <dygons14@gmail.com>
2024-12-11 14:48:36 +0100

    Prepare scripts

--- scripts/fund-wallets.ts
+++ scripts/fund-wallets.ts
@@ -1,19 +1,77 @@
-import { PublicKey } from "@solana/web3.js";
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import {
+  getAssociatedTokenAddressSync,
+  getOrCreateAssociatedTokenAccount,
+  transfer,
+} from "@solana/spl-token";
+import { PublicKey, Keypair } from "@solana/web3.js";
+import fs from "fs";
+require("dotenv").config();
 
-const USDC_MINT = new PublicKey(0);
-const TTS_MINT = new PublicKey(1);
-const USDC_AMOUNT = 100000000;
-const TTS_AMOUNT = 100000000;
-const ETH_AMOUNT = 100000000;
+const provider = AnchorProvider.local(
+  "https://testnet.dev2.eclipsenetwork.xyz"
+);
+const connection = provider.connection;
+
+const USDC_MINT = new PublicKey("5gFSyxjNsuQsZKn9g5L9Ky3cSUvJ6YXqWVuPzmSi8Trx");
+const TTS_MINT = new PublicKey("Ejz1eazd4Nrfy2o2kBSW2a3exk2CT7j2z6yR4QSTCd7i");
+const USDC_AMOUNT = 1e9 * 1.2; // 1.2 USDC
+const TTS_AMOUNT = 1e5 * 1.2; // 1.2 TTS
+
+const FOUNDER = Keypair.fromSecretKey(
+  bs58.decode(process.env.FOUNDER_PRIVATE_KEY as string)
+);
+const FOUNDER_USDC_ATA = getAssociatedTokenAddressSync(
+  USDC_MINT,
+  FOUNDER.publicKey
+);
+const FOUNDER_TTS_ATA = getAssociatedTokenAddressSync(
+  TTS_MINT,
+  FOUNDER.publicKey
+);
 
 const main = async () => {
-  console.log("Funding wallets...");
-  console.log("USDC mint:", USDC_MINT.toBase58());
-  console.log("TTS mint:", TTS_MINT.toBase58());
-  console.log("USDC amount:", USDC_AMOUNT);
-  console.log("TTS amount:", TTS_AMOUNT);
-  console.log("ETH amount:", ETH_AMOUNT);
-  console.log("Funded wallets!");
+  const wallets = JSON.parse(
+    fs.readFileSync("./scripts/wallets.json", "utf-8")
+  );
+  const walletKeys = wallets.map((w) =>
+    Keypair.fromSecretKey(new Uint8Array(w))
+  );
+
+  for (const [index, wallet] of walletKeys.entries()) {
+    console.log(`Funding wallet ${index + 1}`);
+    console.log(wallet.publicKey.toBase58());
+    const usdcAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      FOUNDER,
+      USDC_MINT,
+      wallet.publicKey
+    );
+    const ttsAta = await getOrCreateAssociatedTokenAccount(
+      connection,
+      FOUNDER,
+      TTS_MINT,
+      wallet.publicKey
+    );
+
+    await transfer(
+      connection,
+      FOUNDER,
+      FOUNDER_USDC_ATA,
+      usdcAta.address,
+      FOUNDER,
+      USDC_AMOUNT
+    );
+    await transfer(
+      connection,
+      FOUNDER,
+      FOUNDER_TTS_ATA,
+      ttsAta.address,
+      FOUNDER,
+      TTS_AMOUNT
+    );
+  }
 };
 
 main();
