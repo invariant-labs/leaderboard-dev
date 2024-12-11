014e488302b95b104b7b05970ef5aa8628be57e4
matepal00 <mateusz.paluch2311@gmail.com>
2024-12-11 08:48:35 +0100

    leftovers

--- src/snap-points.ts
+++ src/snap-points.ts
@@ -207,7 +207,7 @@ export const createSnapshotForNetwork = async (network: Network) => {
       return { pool, poolStructure: poolStructure, ticks };
     })
   );
-  console.log(poolsWithTicks);
+
   const currentTimestamp = getTimestampInSeconds();
 
   const convertedLastSnapTimestamp = new BN(lastSnapTimestamp);
