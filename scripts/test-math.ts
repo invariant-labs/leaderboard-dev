a4cdf222a9cad4b7d127f87fb1e752e87e885ad6
matepal00 <mateusz.paluch2311@gmail.com>
2024-12-13 11:26:44 +0100

    math points test

---
+++ scripts/test-math.ts
@@ -0,0 +1,44 @@
+import { Network } from "@invariant-labs/sdk-eclipse";
+import { createSnapshotForNetwork } from "../src/snap-points";
+import { IPointsJson } from "../src/types";
+import * as fs from "fs";
+import path from "path";
+import { BN } from "@coral-xyz/anchor";
+import { POINTS_PER_SECOND } from "../src/math";
+
+const testMath = async () => {
+  const firstTimestamp = await createSnapshotForNetwork(Network.TEST);
+  const previousPointsFirst: Record<string, IPointsJson> = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "../data/points_testnet.json"),
+      "utf-8"
+    )
+  );
+  const pointsFirst: BN = Object.keys(previousPointsFirst).reduce(
+    (acc, curr) =>
+      acc.add(new BN(previousPointsFirst[curr].totalPoints, "hex")),
+    new BN(0)
+  );
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const secondTimestamp = await createSnapshotForNetwork(Network.TEST);
+  const previousPointsSecond: Record<string, IPointsJson> = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "../data/points_testnet.json"),
+      "utf-8"
+    )
+  );
+  const pointsSecond: BN = Object.keys(previousPointsSecond).reduce(
+    (acc, curr) =>
+      acc.add(new BN(previousPointsSecond[curr].totalPoints, "hex")),
+    new BN(0)
+  );
+
+  const pointsDiff = pointsSecond.sub(pointsFirst);
+  const expectedPointsDiff = secondTimestamp
+    .sub(firstTimestamp)
+    .mul(POINTS_PER_SECOND)
+    .muln(10 ** 6);
+  console.log(pointsDiff.toNumber(), expectedPointsDiff.toNumber());
+};
+
+testMath();
